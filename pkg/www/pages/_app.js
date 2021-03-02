6738673758b0c13096b9b5a53397c5d1d1f6925c
toxahak <tohahak540@gmail.com>
2021-03-02 20:20:35 +0200

    fix typerrors suppressed by etask

--- pkg/www/pages/_app.js
+++ pkg/www/pages/_app.js
@@ -1,4 +1,7 @@
 import '../styles/globals.css';
+import zerr from '../../util/zerr.js';
+
+zerr.set_exception_handler = ('www_', (name, n, err)=>{ throw err; });
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
