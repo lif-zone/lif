06d032e3052d1bf3919d567c2ed0dfd8b4b951e9
toxahak <tohahak540@gmail.com>
2021-03-02 20:52:23 +0200

    fix paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-      require('@tailwindcss/forms'),
-  ],
-}
+    purge: ['./pkg/www/pages/**/*.{js,ts,jsx,tsx}',
+        './pkg/www/components/**/*.{js,ts,jsx,tsx}'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+    ],
+};
