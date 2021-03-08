32dac311d9cde9e248cb37c434d4b0527fad4596
lif-rnd <lif.zone.main@gmail.com>
2021-03-08 17:55:23 +0200

    updates to site

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = {
     i18n: {
         defaultLocale: 'he',
-        locales: ['en', 'he'],
-        //localeDetection: false,
+        locales: ['he', 'en'],
+        localeDetection: false,
     },
     localePath: path.resolve('./pkg/www/public/locales/'),
 }
--- pkg/www/pages/index.js
+++ pkg/www/pages/index.js
@@ -1,5 +1,6 @@
 import cn from 'classnames';
-import {forwardRef} from 'react';
+import {forwardRef, Component} from 'react';
+
 import etask from '../../util/etask.js';
 import Layout from '../components/layout.js';
 import Icon_arrow from '../public/img/icon_arrow.svg';
@@ -10,7 +11,9 @@ import {use_app_context} from '../utils/context.js';
 import Link from 'next/link';
 import {useTranslation} from 'next-i18next';
 
-const video_url = 'https://drive.google.com/file/d/1haTxF4aocS6V9FVej21ghdy3ZFetOzba/preview';
+//const video_url = 'https://www.youtube.com/embed/IGtBJ49cX1c?modestbranding=0&rel=0&showinfo=0';
+//const video_url = 'https://player.vimeo.com/video/520902331?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479';
+const video_url = 'https://player.vimeo.com/video/520902331?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479';
 
 const Primary_button = forwardRef(({children, href, arrow, onClick}, ref)=>{
     const {direction} = use_app_context();
@@ -54,12 +57,14 @@ const Home_first = ()=>{
       <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 py-14
         pl-10 pr-14">
         <div className="text-center mb-8 sm:text-right md:pt-4 sm:pe-10">
-          <h1>LIF</h1>
-          <p className="mt-8 text-2xl">{t('title')}</p>
+          <h1>{t('title')}</h1>
+          <p className="mt-8 text-2xl">{t('title2')}</p>
         </div>
         <div>
           <div className="aspect-w-16 aspect-h-9">
-            <iframe src={video_url} allowFullScreen className="shadow-xl"/>
+            <iframe src={video_url} allowFullScreen className="shadow-xl"
+	      frameBorder="0" title="LIF - Liberty, Independence, Freedom"
+              allow="autoplay; fullscreen; picture-in-picture"/>
           </div>
         </div>
       </div>
@@ -70,6 +75,38 @@ export const getStaticProps = ({locale})=>etask(function*(){
     const props = yield serverSideTranslations(locale, ['common', 'homepage']);
     return {props};
 });
+
+class Contact_us extends Component{
+  state = {};
+  render(){
+    const {t} = this.props;
+    const {mode} = this.state;
+    return <div className="contact_us_form">
+      {mode=='enter' ?
+	(<div ref={this.on_ref}>
+	  <input className="lif-input" placeholder={t('name')}/>
+	  <input className="lif-input" placeholder={t('email')}/>
+	  <input className="lif-input" placeholder={t('phone')}/>
+	  <textarea className="lif-textarea" placeholder={t('what_i_can_do')}/>
+	  <Primary_button arrow onClick={this.on_send}>{t('send')}</Primary_button>
+	</div>)
+      : mode=='sent' ?
+	(<div>
+	  <p>{t('thank_you_will_get_back_to_you_soon')}</p>
+          <Primary_button arrow onClick={this.on_click}>{t('contact_us')}</Primary_button>
+        </div>)
+      :
+      <Primary_button arrow onClick={this.on_click}>{t('contact_us')}</Primary_button>
+      }
+    </div>;
+  }
+  on_click = ()=>this.setState({mode: 'enter'});
+  on_send = ()=>this.setState({mode: 'sent'});
+  on_ref = ref=>{
+    if (ref)
+      ref.scrollIntoView();
+  };
+}
 
 export default function Home(){
   const {t} = useTranslation('homepage');
@@ -141,7 +178,7 @@ export default function Home(){
                 <Arrow_link>{t('more_info')}</Arrow_link>
               </Link>
             </div>
-            <div className="px-12 py-6 hidden sm:block border-e">
+            <div className="px-12 py-6 sm:block border-e">
               <div className="flex flex-col items-center mb-4">
                 <img src="/img/digital_money.png" className="w-60"/>
               </div>
@@ -151,7 +188,7 @@ export default function Home(){
                 <Arrow_link>{t('more_info')}</Arrow_link>
               </Link>
             </div>
-            <div className="px-12 py-6 hidden md:block">
+            <div className="px-12 py-6 md:block">
               <div className="flex flex-col items-center mb-4">
                 <img src="/img/get_donations.png" className="w-60"/>
               </div>
@@ -194,8 +231,9 @@ export default function Home(){
               <p>{t('want_to_help_desc1')}</p>
               <p>{t('want_to_help_desc2')}</p>
               <p>{t('want_to_help_desc3')}</p>
+              <p><a href="mailto:help@lif.zone">help@lif.zone</a></p>
 	    </div>
-            <Primary_button arrow>{t('contact_us')}</Primary_button>
+	    <Contact_us t={t}/>
           </div>
         </div>
       </div>
