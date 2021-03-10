1f70438f7a5e0d33b05db6a956db0998d4d1f958
toxahak <tohahak540@gmail.com>
2021-03-10 20:45:56 +0200

    use date instead of ts

--- pkg/www/pages/api/register_contact_us.js
+++ pkg/www/pages/api/register_contact_us.js
@@ -1,7 +1,7 @@
 import mongo from '../../../util/mongo.js';
 
 export default async (req, res)=>{
-    let doc = {...req.body, ts: Date.now()};
+    let doc = {...req.body, ts: new Date()};
     await mongo.insert('contact_us', doc);
     res.status(200).json({done: 1});
 };
