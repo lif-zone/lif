0c051afb82e9ffd4f9824ff86d8a3027a8d5cde7
lif-rnd <lif.zone.main@gmail.com>
2021-03-13 20:18:59 +0200

    use-cases

--- pkg/www/pages/use-cases.js
+++ pkg/www/pages/use-cases.js
@@ -5,19 +5,26 @@ import {serverSideTranslations} from 'next-i18next/serverSideTranslations';
 import {Trans, useTranslation} from 'next-i18next';
 
 export const getStaticProps = ({locale})=>etask(function*(){
-    const props = yield serverSideTranslations(locale, ['common', 'use_case']);
+    const props = yield serverSideTranslations(locale, ['common', 'use-cases']);
     return {props};
 });
 
 export default function Home(){
-    const {t} = useTranslation('use_case');
+    const {t} = useTranslation('use-cases');
     return (
       <Layout>
         <div className="max-w-6xl mx-auto py-14 px-8">
           <h1>{t('title')}</h1>
+          <h2 className="pt-10 font-normal">{t('title2')}</h2>
           <p className="mt-8 text-2xl text-gray-800">{t('p1')}</p>
           <div className="mt-8 text-xl text-gray-600">
             <Trans t={t} i18nKey="p2"/>
+          </div>
+          <div className="mt-8 text-xl text-gray-600 font-bold">
+            <Trans t={t} i18nKey="p3"/>
+          </div>
+          <div className="mt-8 text-xl text-gray-600">
+            <Trans t={t} i18nKey="p4"/>
           </div>
         </div>
       </Layout>
