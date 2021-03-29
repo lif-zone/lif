072ec2f95a8b4c958f5e69557859831ad80f5473
lif-rnd <lif.zone.main@gmail.com>
2021-03-29 19:22:30 +0300

    fix webspark dna link

--- pkg/www/pages/about.js
+++ pkg/www/pages/about.js
@@ -51,7 +51,8 @@ export default function Home(){
 	    <p>
               <Trans t={t} i18nKey="sec4_p2" components={[
 	        <a href='https://hola.org/dna'>, </a>,
-	        <a href='https://luminati.io/dna'>, </a>]}/>
+	        <a href='https://luminati.io/dna'>, </a>,
+	        <a href='https://holaspark.com/dna'>, </a>]}/>
             </p>
 	    <p><Trans t={t} i18nKey="sec4_p3"/></p>
 	    <p><Trans t={t} i18nKey="sec4_p4"/></p>
