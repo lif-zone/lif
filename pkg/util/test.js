5a552e153e0ab1f8fc99a76b50393eac5e6665de
lif-rnd <lif.zone.main@gmail.com>
2021-04-01 19:17:29 +0300

    add mocha + simple test

---
+++ pkg/util/test.js
@@ -0,0 +1,26 @@
+// LICENSE_CODE LIF
+'use strict';
+const LIF = require('./lif.js');
+
+var assert = require('assert');
+describe('Scroll', function() {
+  describe('api', function() {
+    it('basic', function() {
+      let s = LIF.Scroll.create();
+      // XXX: temporary hack, use sinon to control time
+      Date.now = function(){ return 100; }
+      s.write({sub_type: 'start', text: 'Hello LIF'});
+      s.write({text: 'I am a free man'} );
+      s.write({sub_type: 'close', text: 'And you?'} );
+      s.close();
+      assert.deepEqual(s.lines, [
+        {sig: 1234, json: {ts: 100, type: 'scroll', sub_type: 'start',
+          text: 'Hello LIF'}},
+        {sig: 1234, json: { ts: 100, type: 'scroll', sub_type: 'line',
+          text: 'I am a free man' }},
+        {sig: 1234, json: {ts: 100, type: 'scroll', sub_type: 'close',
+          text: 'And you?'}}
+      ]);
+    });
+  });
+});
