5cec626d91a271d067bac601e377a688a7839254
lif-rnd <lif.zone.main@gmail.com>
2021-04-01 22:31:02 +0300

    fix lint

--- pkg/util/test.js
+++ pkg/util/test.js
@@ -3,21 +3,21 @@
 const LIF = require('./lif.js');
 
 var assert = require('assert');
-describe('Scroll', function() {
-  describe('api', function() {
-    it('basic', function() {
+describe('Scroll', function(){
+  describe('api', function(){
+    it('basic', function(){
       let s = LIF.Scroll.create();
       // XXX: temporary hack, use sinon to control time
-      Date.now = function(){ return 100; }
+      Date.now = function(){ return 100; };
       s.write({sub_type: 'start', text: 'Hello LIF'});
-      s.write({text: 'I am a free man'} );
-      s.write({sub_type: 'close', text: 'And you?'} );
+      s.write({text: 'I am a free man'});
+      s.write({sub_type: 'close', text: 'And you?'});
       s.close();
       assert.deepEqual(s.lines, [
         {sig: 1234, json: {ts: 100, type: 'scroll', sub_type: 'start',
           text: 'Hello LIF'}},
-        {sig: 1234, json: { ts: 100, type: 'scroll', sub_type: 'line',
-          text: 'I am a free man' }},
+        {sig: 1234, json: {ts: 100, type: 'scroll', sub_type: 'line',
+          text: 'I am a free man'}},
         {sig: 1234, json: {ts: 100, type: 'scroll', sub_type: 'close',
           text: 'And you?'}}
       ]);
