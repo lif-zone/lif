6d23576851d23d5b7a9adcea06790901c2c1044d
lif-rnd <lif.zone.main@gmail.com>
2021-04-03 13:17:39 +0300

    fix name

--- pkg/util/test.js
+++ pkg/util/test.js
@@ -4,27 +4,27 @@ const Scroll = LIF.Scroll;
 
 describe('Scroll', function(){
 it('http_site', function(){
-  // XXX sesom: create/load LIF.Wallet
+  // XXX: create/load LIF.Wallet
   let god = Scroll.create();
-  god.declare({type: 'scroll', faith: 'sesom', tag: 'faith'});
+  god.declare({type: 'scroll', faith: 'john', tag: 'faith'});
   god.declare({type: 'faith', type_on: 'http',
     def: {file_ext: {'.jpg': {'content-type': 'image/jpeg'}}}});
   god.declare({type: 'faith', type_on: 'http',
     def: {uri: {'/index.html': {homepage: true}}}});
-  god.declare({type: 'domain', domain: 'sesom@lif'});
+  god.declare({type: 'domain', domain: 'john@lif'});
   let s = Scroll.create();
-  s.declare({type: 'scroll', faith: 'sesom', tag: 'http'});
-  s.declare({type: 'http', domain: 'sesom@lif', uri: '/index.html',
-    content: `<html><body><img src="lif://sesom@lif/sesom.jpg"/>
+  s.declare({type: 'scroll', faith: 'john', tag: 'http'});
+  s.declare({type: 'http', domain: 'john@lif', uri: '/index.html',
+    content: `<html><body><img src="lif://john@lif/john.jpg"/>
       </body></html>`});
-  s.declare({type: 'http', domain: 'sesom@lif', uri: '/sesom.jpg',
+  s.declare({type: 'http', domain: 'john@lif', uri: '/john.jpg',
     blob: 'XXX'});
 /*
-Q. how to access sesom@lif?
-1. through http gateway https://sesom.lif.zone/
+Q. how to access john@lif?
+1. through http gateway https://john.lif.zone/
 2. through LIF client
 
-Q. how to find sesom@lif?
+Q. how to find john@lif?
 - using an index 'by domain' service
 - an index service will read all scroll and maitain index of all the uri
   of a domain. the index will keep checksum for each uri.
