9ec334a7e4fe8ed16f2caedd853ca13d476a871f
lif-rnd <lif.zone.main@gmail.com>
2021-04-07 14:46:19 +0300

    fix bug in submit

--- pkg/www/pages/api/israeltomorrow_save_email.js
+++ pkg/www/pages/api/israeltomorrow_save_email.js
@@ -1,8 +1,9 @@
 import mongo from '../../../util/mongo.js';
 
 export default async (req, res)=>{
+  let email = (''+(req.body && req.body.email)).substr(0, 256);
   await mongo.insert('israeltomorrow_emails', {ts: new Date(),
-    email: doc.email, user_agent: req.headers['user-agent']});
+    email: email, user_agent: req.headers['user-agent']});
   res.setHeader('Access-Control-Allow-Origin',
     'https://israeltomorrow.co.il');
   res.status(200).json({done: 1});
