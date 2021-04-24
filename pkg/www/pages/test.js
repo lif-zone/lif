860db25b133312a1185996de1b0c6201ef97f34e
lif-rnd <lif.zone.main@gmail.com>
2021-04-24 15:02:54 +0300

    add hyper-sdk-bundle.js

--- pkg/www/pages/test.js
+++ pkg/www/pages/test.js
@@ -4,6 +4,7 @@ import Layout from '../components/layout.js';
 export default function Scroll_page(){
   return (
     <Layout>
+      <script src="/js/hyper-sdk-bundle.js"/>
       <script src='/js/test.js'/>
       <div className="px-3">
         <h3>Test</h3>
