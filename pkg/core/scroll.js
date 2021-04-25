f4f936ec21c4f2a2a8f11db957bc46e6c4974fd9
toxahak <tohahak540@gmail.com>
2021-04-25 17:47:42 +0300

    /wallet based on local hypercore feeds

--- pkg/core/scroll.js
+++ pkg/core/scroll.js
@@ -1,39 +1,132 @@
 // LICENSE_CODE LIF
 'use strict';
-import zutil from '../util/util.js';
 import date from '../util/date.js';
-import {sign, key_to_str} from './wallet.js';
+import {sign, key_to_str, str_to_key} from './wallet.js';
+import crypto from 'hypercore-crypto';
+import {toPromises} from 'hypercore-promisifier';
+import hypercore from 'hypercore';
+import rai from 'random-access-idb';
 
 let E = {};
 
 class Scroll {
-    constructor(conn, sid){
-        this.conn = conn;
-        this.sid = sid;
+    constructor(feed){
+        this.feed = feed;
     }
-    async decl(keypair, data, blob){
-        const {idx, prev_sig} = await this.conn.lock();
-        const meta = {idx, ts: E.ts(), prev_sig,
-            public_key: await key_to_str(keypair.public_key)};
-        if (blob) // XXX: use base64 instead
-            meta.blob = zutil.buf2hex(blob);
-        let str_to_sign = JSON.stringify(meta)+JSON.stringify(data);
-        const sig = await sign(zutil.str2ab(str_to_sign), keypair.private_key);
-        await this.conn.insert({sig, meta, data});
-        await this.conn.unlock();
+    async decl(data, blob){
+        const meta = {idx: this.feed.length, ts: E.ts()};
+        if (blob) // XXX: use base64 instead?
+            meta.blob = blob.toString('hex');
+        await this.feed.append({meta, data});
     }
-    find_one(query){
-        return this.conn.find_one(query);
+    async get(index){
+        return await this.feed.get(index);
     }
-    find_all(query){
-        return this.conn.find_all(query);
+    async find_all(query){
+        // XXX: use 'query'
+        let declarations = [];
+        for (let i=0; i<this.feed.length; i++)
+            declarations.push(await this.feed.get(i));
+        return declarations;
     }
-    close(){}
+    get_length(){
+        return this.feed.length;
+    }
+    close(){
+        this.feed.close();
+    }
 }
 
-E.open = async (impl, sid)=>{
-    let conn = await impl.connect(sid);
-    return new Scroll(conn, sid);
+// for wallet UI only: store user's scrolls in localStorage as name=>keypair
+let save_my_scroll = (name, keypair)=>{
+    let json = localStorage.getItem('lif_my_scrolls');
+    let my_scrolls = json ? JSON.parse(json) : [];
+    my_scrolls.push({public_key: key_to_str(keypair.public_key),
+        private_key: key_to_str(keypair.private_key)});
+    localStorage.setItem('lif_my_scrolls', JSON.stringify(my_scrolls));
+};
+
+// for wallet UI only
+E.get_all_scrolls = async ()=>{
+    let json = localStorage.getItem('lif_my_scrolls');
+    let my_scrolls = json ? JSON.parse(json) : [];
+    let scrolls = [];
+    for (let s of my_scrolls)
+    {
+        let {public_key, private_key} = s;
+        let keypair = {public_key: str_to_key(public_key),
+            private_key: str_to_key(private_key)};
+        let scroll = await E.open_write(keypair);
+        let length = scroll.get_length();
+        let info = {length, public_key};
+        if (length)
+        {
+            let first_decl = await scroll.get(0);
+            info.created_at = first_decl && first_decl.meta &&
+                first_decl.meta.ts;
+            info.name = first_decl && first_decl.data &&
+                first_decl.data.author && first_decl.data.author.name;
+            let last_decl = await scroll.get(length-1);
+            info.updated_at = last_decl && last_decl.meta &&
+                last_decl.meta.ts;
+        }
+        scrolls.push(info);
+    }
+    return scrolls.sort((a, b)=>a.updated_at<b.updated_at ? 1 : -1);
+};
+
+// for wallet UI only
+E.find_my_scroll_keypair = public_key_str=>{
+    let json = localStorage.getItem('lif_my_scrolls');
+    let my_scrolls = json ? JSON.parse(json) : [];
+    for (let s of my_scrolls)
+    {
+        let {public_key, private_key} = s;
+        if (public_key!=public_key_str)
+            continue;
+        return {public_key: str_to_key(public_key),
+            private_key: str_to_key(private_key)};
+    }
+};
+
+E.open_write = async scroll_keypair=>{
+    let idb = rai('lif_'+key_to_str(scroll_keypair.public_key));
+    let feed = toPromises(hypercore(filename=>{
+        return idb(filename);
+    }, scroll_keypair.public_key, {
+        valueEncoding: 'json',
+        storeSecretKey: false,
+        secretKey: scroll_keypair.private_key,
+    }));
+    await feed.ready();
+    return new Scroll(feed);
+};
+
+E.open_read = async public_key=>{
+    if (typeof public_key=='string')
+        public_key = str_to_key(public_key, 'hex');
+    let idb = rai('lif_'+key_to_str(public_key));
+    let feed = toPromises(hypercore(filename=>{
+        return idb(filename);
+    }, public_key, {
+        valueEncoding: 'json',
+        storeSecretKey: false,
+    }));
+    await feed.ready();
+    return new Scroll(feed, name);
+};
+
+E.create = async (name, wallet_keypair)=>{
+    const {publicKey: public_key, secretKey: private_key} = crypto.keyPair();
+    let scroll = await E.open_write({public_key, private_key});
+    let name_sig = sign(Buffer.from(name), wallet_keypair.private_key);
+    let first_decl = {
+        author: {name, pub: key_to_str(wallet_keypair.public_key),
+            sig: name_sig},
+    };
+    await scroll.decl(first_decl);
+    save_my_scroll(name, {public_key, private_key});
+    return scroll;
 };
 
 E.ts = function(){
--- pkg/core/wallet.js
+++ pkg/core/wallet.js
@@ -1,72 +1,49 @@
 // LICENSE_CODE LIF
 'use strict';
-import etask from '../util/etask.js';
-import zutil from '../util/util.js';
+import crypto from 'hypercore-crypto';
+import {Buffer} from 'buffer';
 
-const ALGORITHM = 'RSA-PSS', HASH = 'SHA-256';
 let E = {};
 
-const str_to_key = str=>etask(function*(){
-    try {
-        const json = JSON.parse(str);
-        return yield window.crypto.subtle.importKey('jwk', json,
-            {name: ALGORITHM, hash: HASH}, true, json.key_ops);
-    } catch(e){
-        console.error(e);
-    }
-});
+export const str_to_key = str=>{
+    return Buffer.from(str, 'hex');
+};
 
-export const key_to_str = key=>etask(function*(){
-    try {
-        const jwk = yield window.crypto.subtle.exportKey('jwk', key);
-        return JSON.stringify(jwk);
-    } catch(e){
-        console.error(e);
-    }
-});
+export const key_to_str = key=>{
+    return key.toString('hex');
+};
 
-const store_key = (name, key)=>etask(function*(){
-    const raw_key = yield key_to_str(key);
+const store_key = (name, key)=>{
+    const raw_key = key_to_str(key);
     if (raw_key)
         localStorage.setItem(`lif_${name}`, raw_key);
-});
+};
 
-export const import_keys = ()=>etask(function*(){
+export const import_keys = ()=>{
     const public_key_raw = localStorage.getItem('lif_public_key');
     const private_key_raw = localStorage.getItem('lif_private_key');
     if (!public_key_raw || !private_key_raw)
         return;
-    const public_key = yield str_to_key(public_key_raw);
-    const private_key = yield str_to_key(private_key_raw);
+    const public_key = str_to_key(public_key_raw);
+    const private_key = str_to_key(private_key_raw);
     if (!public_key || !private_key)
         return;
     return {public_key, private_key};
-});
+};
 
-export const sign = (buf, private_key)=>etask(function*(){
-    try {
-        const signature = yield window.crypto.subtle.sign(
-            {name: ALGORITHM, saltLength: 128}, private_key, buf);
-        return zutil.buf2hex(signature);
-    } catch(e){ console.error(e); }
-});
+export const sign = (buf, private_key)=>{
+    return crypto.sign(buf, private_key).toString('hex');
+};
 
-export const generate_keys = ()=>etask(function*(){
-    try {
-        const keys = yield window.crypto.subtle.generateKey({
-            name: ALGORITHM,
-            modulusLength: 4096,
-            publicExponent: new Uint8Array([1, 0, 1]),
-            hash: HASH,
-        }, true, ['sign', 'verify']);
-        return {public_key: keys.publicKey, private_key: keys.privateKey};
-    } catch(e){ console.error(e); }
-});
+export const generate_keys = ()=>{
+    const {publicKey: public_key, secretKey: private_key} = crypto.keyPair();
+    return {public_key, private_key};
+};
 
-export const generate_and_store_keys = ()=>etask(function*(){
-    let {public_key, private_key} = yield generate_keys();
-    yield store_key('public_key', public_key);
-    yield store_key('private_key', private_key);
-});
+export const generate_and_store_keys = ()=>{
+    let {public_key, private_key} = generate_keys();
+    store_key('public_key', public_key);
+    store_key('private_key', private_key);
+};
 
 export default E;
