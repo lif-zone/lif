dc1d1bf145f19630d9f1c58df060e7e74fa63864
lif-rnd <lif.zone.main@gmail.com>
2021-11-18 13:52:24 +0200

    add poc

---
+++ pkg/www/pages/poc/index.js
@@ -0,0 +1,12 @@
+'use strict'; /*jslint node:true, browser:true, react: true*/
+import Layout from '../../components/layout.js';
+
+export default function Wallet(){
+  return (
+    <Layout>
+      <div className="px-3">
+        POC
+      </div>
+    </Layout>
+  );
+}
