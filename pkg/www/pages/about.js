c34d25415ce8c47b69e6070f72ff2fc75221367b
lif-rnd <lif.zone.main@gmail.com>
2022-03-15 11:18:33 +0200

    fix typo

--- pkg/www/pages/about.js
+++ pkg/www/pages/about.js
@@ -57,8 +57,10 @@ export default function Home(){
 	    <p>
               <Trans t={t} i18nKey="sec4_p2" components={[
 	        <a href='https://hola.org/dna'>, </a>,
+	        <a href='https://holaspark.com/dna'>, </a>,
+	        <a href='https://holacdn.com/dna'>, </a>,
+	        <a href='https://holavpn.com/dna'>, </a>]}/>
 	        <a href='https://luminati.io/dna'>, </a>,
-	        <a href='https://holaspark.com/dna'>, </a>]}/>
             </p>
 	    <p><Trans t={t} i18nKey="sec4_p3"/></p>
 	    <p><Trans t={t} i18nKey="sec4_p4"/></p>
