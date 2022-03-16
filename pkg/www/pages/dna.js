554913d25719c4313c7102602b49f67a4769b3f1
lif-rnd <lif.zone.main@gmail.com>
2022-03-16 14:46:33 +0200

    coding

--- pkg/www/pages/dna.js
+++ pkg/www/pages/dna.js
@@ -1,60 +1,14 @@
-import {useEffect} from 'react';
 import Layout from '../components/layout.js';
+import {dna_style} from '../components/style.js';
 import {H1, H2, H3} from '../components/anchor.js';
 import {Primary_button, Footer, Video} from '../components/common.js';
 const NL = '\n';
 
-// XXX: mv to css
-const page_style=<style>{`
-.dna-page h1 {padding-top: 1.2rem; padding-bottom: 0.5rem;}
-.dna-page h2 {padding-top: 1.2rem; padding-bottom: 0.2rem;}
-.dna-page h3 {padding-top: 1.2rem; padding-bottom: 0.1rem;}
-.dna-page p {padding-top: 0.1rem; padding-bottom: 0.1rem}
-.dna-page ul {padding-top: 0.1rem; padding-bottom: 0.1rem; margin: 0;}
-.dna-page li {list-style-position: outside; margin-left: 1rem;}
-.dna-page .bad, .dna-page [cat=bad] {background-color: rgba(255,0,0,0.1);
-  border-color: rgba(255,0,0,0.7);}
-.dna-page .ok, .dna-page[cat=ok] {background-color: rgba(0,255,0,0.1);
-  border-color: rgba(48,48,48,0.7);}
-.dna-page .good, .dna-page [cat=good] {background-color: rgba(0,255,0,0.1);
-  border-color: rgba(0,255,0,0.7);}
-.dna-page .better, .dna-page [cat=better]{
-  background-color: rgba(0,255,0,0.25); border-color: rgba(0,255,0,0.7);}
-.dna-page .ok,[cat=ok], .dna-page .good, .dna-page [cat=good],
-  .dna-page .better, .dna-page [cat=better], .dna-page .bad,
-  .dna-page [cat=bad] {
-  padding: 4px; margin-bottom: 10px; border-width: 0 3px;
-  border-style: solid; border-radius: 4px; position: relative;}
-.dna-page th, .dna-page td {border: 1px solid black;
-  border-collapse: collapse;}
-.dna-page table {background-color: transparent;}
-.dna-page table {border-spacing: 0; border-collapse: collapse;}
-.dna-page pre {overflow: auto;}
-`}</style>;
-
 export default function DNA(){
-  let current;
-  useEffect(()=>{
-    let hash = location.hash, timer, timer2;
-    if (hash && current!=hash){
-      current = hash;
-      clearTimeout(timer);
-      clearTimeout(timer2);
-      let id = hash.replace('#', '');
-      let el = document.getElementById(id);
-      if (el){
-        el.scrollIntoView({behavior: 'smooth'});
-        timer = setTimeout(()=>el.scrollIntoView(), 100);
-        timer2 = setTimeout(()=>el.scrollIntoView(), 250);
-        current = hash;
-      }
-    }
-  });
-  return (
-    <Layout title='LIF DNA' desc='LIF DNA'>
-{page_style}
+  Layout.use_scroll_to_hash();
+return (
+<Layout title='LIF DNA' desc='LIF DNA' style={dna_style}>
 <div className="dna-page max-w-6xl mx-auto px-6 pb-10">
-
 <h1>The DNA</h1>
 <div>XXX - Add missing header</div>
 <div>XXX - Fix From: and new line in sections</div>
@@ -2400,7 +2354,5 @@ Niv
   Let's not do this bandwidth saving feature. Our competitors don't have it,
   and it will cause our customers to stream less and us to make less revenues.
 </div>
-
-</div></Layout>
-    );
+</div></Layout>);
 }
